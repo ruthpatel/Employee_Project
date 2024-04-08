98506147bbe3146b25a17935a4b288e03cf0f0fb
ANMOLSINGH3535 <162834024+ANMOLSINGH3535@users.noreply.github.com>
2024-04-07 21:25:27 -0400

    Anmol singh

--- __init__.py
+++ __init__.py
@@ -1,15 +1,20 @@
-"""
-Employee Management System Module
+def update_employee_data(employee_id, employees=None):
+    if employee_id not in employees:
+        print(f"No employee was found with ID {employee_id}.")
+        return
 
-This module provides various functionalities for managing employee data.
-"""
+    employee = employees[employee_id]
 
-# Define the list of modules to be imported when the package is imported
-# This list specifies the modules that will be imported when using "from package import *"
-# In this case, only the main_menu module is exposed to the user
-__all__ = ['menu']
+    # Update employee attributes
+    employee.first_name = input("Enter the new first name: ")
+    employee.last_name = input("Enter the new last name: ")
+    employee.date_of_birth = input("Enter the new date of birth (MM/DD/YYYY): ")
+    employee.department = input("Enter the new department: ")
+    employee.salary = float(input("Enter the new salary: "))
 
-# Import the main_menu module from the src.menu submodule
-# This import statement allows the main_menu function to be accessed directly from the package
-# Users can use "from package import main_menu" to access the main_menu function
-from .src.menu import menu
+    # Save updated employee dictionary to text file
+    with open("employees.txt", "w") as f:
+        for employee_id, employee in employees.items():
+            f.write(f"{employee_id},{employee.first_name},{employee.last_name},{employee.date_of_birth},{employee.department},{employee.salary}\n")
+
+    print(f"Employee with ID {employee_id} has been updated.")
